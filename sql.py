cb1c05a1f86165b476ae421672a194e03bda62c4
Farthing <mhel.farthing@outlook.com>
2019-02-17 19:14:55 +0800

    update commit.

--- sql.py
+++ sql.py
@@ -7,7 +7,7 @@ user = 'CNTrain'
 password = 'password'
 db = 'CNTrain'
 port=3306
-#########
+##########
 
 class sql(object):
     def __init__(self,host= '127.0.0.1',port=3306,user='CNTrain',password= 'password',dbname= 'CNTrain'):
