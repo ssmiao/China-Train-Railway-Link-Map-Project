9943723ddb975663c2d9d33326c755dbe18fb98b
Farthing <mhel.farthing@outlook.com>
2019-02-23 12:50:27 +0800

    bug fixed

--- station.py
+++ station.py
@@ -80,7 +80,7 @@ class station(object):
         while True:
             try:
                 response = requests.post(url, params, timeout=1).json()
-            except :(requests.exceptions.Timeout,json.JSONDecodeError):
+            except (requests.exceptions.Timeout,json.JSONDecodeError):
                 pass
             else:
                 break
